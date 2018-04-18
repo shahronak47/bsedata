0fa07cf615ac3dc8a2e69816d4d371d7e28f0051
Ronak Shah <shahronak47@yahoo.in>
2018-04-18 22:40:18 +0800

    Added change from yesterday

--- bsedata.py
+++ bsedata.py
@@ -1,34 +1,36 @@
 from bsetools import bsetools
 import pandas as pd
 from email_file import email_main
-import pdb
 
 #path of the csv file
 CSV_FILE_PATH = "C:\\Users\\Ronak Shah\\Google Drive\\Documents\\Shares.csv"
 
 def get_current_stock_prices(share_list, bse) :
     current_data_list = []
+    diff_list = []
     for share in share_list :
         current_data = bse.get_quote(share)
-        #If the code is valid and we have the data take the 1st index, 2nd is change from yesterday
+        #If the code is valid and we have the data take the 1st index, 2nd is difference from yesterday
         if current_data is not None :
             current_data_list.append(current_data[0])
+            diff_list.append(current_data[1])
         else :
             #If we do not have the data we append 0
             current_data_list.append(0)
+            diff_list.append(0)
 
-    return current_data_list
+    return current_data_list, diff_list
 
 if __name__ == '__main__' :
     # Read the csv file which has data
     df = pd.read_csv(CSV_FILE_PATH)
     #Initialise the bsetools object
     bse = bsetools.bsetools()
-    current_data_list = get_current_stock_prices(df['Shares'].tolist(), bse)
+    current_data_list, diff_list = get_current_stock_prices(df['Shares'].tolist(), bse)
     # Add new columns to dataframe coverting string to float value
     df['Current_price'] = [float(x) for x in current_data_list]
+    df['Change'] = diff_list
     df['Profit'] = (df.Current_price * df.Quantity) - df.Amount
-
     #Get BSE index
     obj = bse.get_BSE_index()
     #pdb.set_trace()
